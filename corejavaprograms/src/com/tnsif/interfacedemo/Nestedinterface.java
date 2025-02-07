b1e0bf77efccf80db08752285e27a4be1d6b7a00
Sai Bharath <Sai Bharath@DESKTOP-FEMEF3B.mshome.net>
2025-02-07 10:48:07 +0530

    first

---
+++ corejavaprograms/src/com/tnsif/interfacedemo/Nestedinterface.java
@@ -0,0 +1,14 @@
+package com.tnsif.interfacedemo;
+
+public class Nestedinterface implements Myinterface.Myinnerinterface{
+
+	@Override
+	public void print() {
+		System.out.println("inner interface method");
+		
+	}
+
+}
+
+
+
